63a4fe0094e532bea87bf746f4686eb5e60e7537
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-21 11:33:23 -0300

    ultimo

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+const axios = require('axios');
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  password: "",
-  database: "banco-solar",
+  user: 'postgres',
+  host: 'localhost',
+  password: '',
+  database: 'banco-solar',
   port: 5432,
   max: 20,
   idleTimeoutMillis: 5000,
   connectionTimeoutMillies: 2000,
 });
+pool.connect()
 
 const guardarUsuario = async (usuario) => {
   const values = Object.values(usuario)
   const consulta = {
-    text: "INSERT INTO usuarios (nombre, balance) VALUES ($1, $2)",
+    text: 'INSERT INTO usuarios (nombre, balance) VALUES ($1, $2)',
     values,
   }
   try {
@@ -27,7 +29,7 @@ const guardarUsuario = async (usuario) => {
 };
 
 const getUsuarios = async () => {
-  const result = await pool.query("SELECT * from usuarios");
+  const result = await pool.query('SELECT * from usuarios');
   return result.rows
 };
 
@@ -35,7 +37,7 @@ const editUsuario = async (usuario) => {
   const values = Object.values(usuario)
   console.log(values)
   const consulta = {
-    text: "UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *",
+    text: 'UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *',
     values,
   }
   try {
@@ -52,4 +54,101 @@ const eliminarUsuario = async (id) => {
   return result.rows
 };
 
-module.exports = { guardarUsuario, getUsuarios, editUsuario, eliminarUsuario }
+/*
+const nuevaTransferencia = async (emisor, receptor, monto ) => {
+  pool.connect( async (error_conexion, client, release) => {
+
+    if(error_conexion) {
+      console.log(error_conexion);
+    } else {
+
+      try {
+        await client.query('BEGIN');
+        const sacar = `UPDATE usuarios SET balance = balance - ${monto} WHERE id = ${emisor} RETURNING *;`;
+        const descuento = await client.query(sacar);
+        console.log('DESCUENTO', descuento)
+        
+        
+
+        const abonar = `UPDATE usuarios SET balance = balance + ${monto} WHERE id = ${receptor} RETURNING *;`;
+        const acreditacion = await client.query(abonar);
+        console.log('ACREDITACION', acreditacion)
+
+        const date = new Date();
+        const ajuste = `INSERT INTO transferencias (emisor,receptor,monto) VALUES (${emisor},${receptor},${monto}) RETURNING *;`;
+        const transaccion = await client.query(ajuste);
+        console.log('TRANSACCION', transaccion)
+
+        console.log("Descuento realizado con éxito: ", descuento.rows[0]);
+        console.log("Acreditación realizada con éxito: ", acreditacion.rows[0]);
+        console.log("Transacción realizada con éxito: ", transaccion.rows[0]);
+        
+        await client.query('COMMIT');
+
+      } catch (e) {
+        await client.query('ROLLBACK');
+  
+        console.log("Error código: " + e.code);
+        console.log("Detalle del error: " + e.detail);
+        console.log("Tabla originaria del error: " + e.table);
+        console.log("Restricción violada en el campo: " + e.constraint);
+        console.log("Error: ", e);
+      }
+      release();
+      pool.end();
+    }
+      
+  })
+}*/
+
+const nuevaTransferencia = async (datos) => {
+
+  try {
+    const { data } = await axios.get("http://localhost:3000/usuarios");
+    const emisor = data.filter((elemento) => {
+      return elemento.id == datos[0];
+    })
+
+    const receptor = data.filter((elemento) => {
+      return elemento.id == datos[1];
+    })
+
+    const result = await pool.query( // si te escucho
+    `
+    BEGIN;
+    UPDATE usuarios SET balance = balance - ${datos[2]} WHERE nombre = '${datos[0]}';
+    UPDATE usuarios SET balance = balance + ${datos[2]} WHERE nombre = '${datos[1]}';
+    INSERT INTO transferencias (emisor,receptor,monto,fecha)
+    VALUES (${emisor[0].id},${receptor[0].id},'${datos[2]}',now());
+    COMMIT;
+    `
+    );
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+const consultarTransferencias = async () => {
+  try {
+      const result = await pool.query(`
+        SELECT t.id, t.monto, t.fecha, em.nombre as emisor, re.nombre as receptor  FROM transferencias t
+        INNER JOIN usuarios em ON em.id = t.emisor
+        INNER JOIN usuarios re ON re.id = t.receptor
+      `);
+      return result.rows;
+  } catch (error) {
+      console.log(error.code);
+      return error;
+  }
+}
+
+pool.connect(async(error_conexion, client, release) => {
+  await client.query('BEGIN');
+
+});
+
+
+
+
+module.exports = { guardarUsuario, getUsuarios, editUsuario, eliminarUsuario, nuevaTransferencia, consultarTransferencias }
--- servidor.js
+++ servidor.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url')
 
-const { guardarUsuario, getUsuarios, editUsuario, eliminarUsuario } = require('./script')
+const { guardarUsuario, getUsuarios, editUsuario, eliminarUsuario, nuevaTransferencia, consultarTransferencias } = require('./script')
 
 http.createServer(async (req, res) => {
 
@@ -18,12 +18,12 @@ http.createServer(async (req, res) => {
     });
   }
   //agregar un nuevo usuario
-  if (req.url == "/usuario" && req.method == "POST") {
-    let body = "";
-    req.on("data", (chunck) => {
+  if (req.url == '/usuario' && req.method == 'POST') {
+    let body = '';
+    req.on('data', (chunck) => {
       body = chunck.toString();
     });
-    req.on("end", async () => {
+    req.on('end', async () => {
       const usuario = JSON.parse(body);
       try {
         const result = await guardarUsuario(usuario);
@@ -31,27 +31,27 @@ http.createServer(async (req, res) => {
         res.end(JSON.stringify(result));
       } catch (e) {
         res.statusCode = 500;
-        res.end("Ocurrio un dilema en el servidor. " + e);
+        res.end('Ocurrio un dilema en el servidor. ' + e);
       }
     });
   }
   //muestra el nuevo usuario en la tabla correspondiente
-  if (req.url == "/usuarios" && req.method == "GET") {
+  if (req.url == '/usuarios' && req.method == 'GET') {
     try {
       const usuarios = await getUsuarios();
       res.end(JSON.stringify(usuarios));
     } catch (e) {
       res.statusCode = 500;
-      res.end("Ocurrio un dilema en el servidor. " + e);
+      res.end('Ocurrio un dilema en el servidor. ' + e);
     }
   }
   //edita un usuario
-  if (req.url.startsWith("/usuario") && req.method == "PUT") {
-    let body = "";
-    req.on("data", (chunck) => {
+  if (req.url.startsWith('/usuario') && req.method == 'PUT') {
+    let body = '';
+    req.on('data', (chunck) => {
       body = chunck.toString();
     });
-    req.on("end", async () => {
+    req.on('end', async () => {
       let { id } = url.parse(req.url, true).query;
 
       const usuario = JSON.parse(body);
@@ -61,20 +61,40 @@ http.createServer(async (req, res) => {
         res.end(JSON.stringify(result));
       } catch (e) {
         res.statusCode = 500;
-        res.end("Ocurrio un dilema en el servidor. " + e);
+        res.end('Ocurrio un dilema en el servidor. ' + e);
       }
     });
   }
   //eliminar un usuario 
-  if (req.url.startsWith("/usuario?id") && req.method == "DELETE") {
+  if (req.url.startsWith('/usuario?id') && req.method == 'DELETE') {
     try {
       let { id } = url.parse(req.url, true).query;
       await eliminarUsuario(id);
-      res.end("Candidato eliminado");
+      res.end('Candidato eliminado');
     } catch (e) {
       res.statusCode = 500;
-      res.end("Ocurrio un dilema en el servidor. " + e);
+      res.end('Ocurrio un dilema en el servidor. ' + e);
     }
   }
+
+  if (req.url == '/transferencia' && req.method == 'POST') {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    })
+
+    req.on('end', async () => {
+      const datos = Object.values(JSON.parse(body));
+      const respuesta = await nuevaTransferencia(datos);
+      res.end(JSON.stringify(respuesta, null, 1));
+    })
+  }
+
+  if (req.url == "/transferencias" && req.method == "GET") {
+    const registros = await consultarTransferencias();
+    res.end(JSON.stringify(registros, null, 1));
+  }
+
 })
-  .listen(3000, () => { console.log("Puerto conectado mi mlady") })
+  .listen(3000, () => { console.log('Puerto conectado mi mlady') })
